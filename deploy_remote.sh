bb353e16b52013583f89e4e682dfa9d9440a81d6
Carsten Brandt <mail@cebe.cc>
2019-11-18 23:53:04 +0100

    "frontend" dir added to deployment script

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -9,16 +9,16 @@ echo "done."
 
 # frontend
 echo "composer-asset-plugin LUYA"
-cd /var/www/users/domi/content/domi.cc
+cd /var/www/users/domi/content/domi.cc/frontend
 su -s /bin/bash -c 'php7 /usr/local/bin/composer global require --ansi fxp/composer-asset-plugin:~1.4' domi || (echo "failed" && exit 1)
 echo "done."
 echo "composer install LUYA"
-cd /var/www/users/domi/content/domi.cc
+cd /var/www/users/domi/content/domi.cc/frontend
 su -s /bin/bash -c 'php7 /usr/local/bin/composer install --no-dev --ansi' domi || (echo "failed" && exit 1)
 echo "done."
 echo "Ensure local config"
-su -s /bin/bash -c 'echo "<?php return require \"env-prod.php\";" > /var/www/users/domi/content/domi.cc/configs/env.php' domi || (echo "failed" && exit 1)
-su -s /bin/bash -c 'echo "<?php return [];" > /var/www/users/domi/content/domi.cc/configs/env-local-db.php' domi || (echo "failed" && exit 1)
+su -s /bin/bash -c 'echo "<?php return require \"env-prod.php\";" > /var/www/users/domi/content/domi.cc/frontend/configs/env.php' domi || (echo "failed" && exit 1)
+su -s /bin/bash -c 'echo "<?php return [];" > /var/www/users/domi/content/domi.cc/frontend/configs/env-local-db.php' domi || (echo "failed" && exit 1)
 echo "done."
 echo "LUYA migrate"
 su -s /bin/bash -c 'php7 ./luya migrate/up --interactive=0' domi || (echo "failed" && exit 1)
